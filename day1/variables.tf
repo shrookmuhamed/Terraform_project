f6d956f624d29c6a2d072122e735eb873f9106c0
shrookmuhamed <152062028+shrookmuhamed@users.noreply.github.com>
2024-05-06 17:00:27 +0300

    Update variables.tf

--- day1/variables.tf
+++ day1/variables.tf
@@ -1,25 +1,19 @@
 variable vpc_cidr {
   type = string
-  description = "description"
 }
 variable pub_subnet_cidr {
   type = string
-  description = "description"
 }
 variable priv_subnet_cidr {
   type = string
-  description = "description"
 }
 variable priv_subnet2_cidr {
   type = string
-  description = "description"
 }
 variable region {
   type = string
-  description = "description" 
   }
 
 variable  verified_email {
   type = string
-  description = "description"
 }
