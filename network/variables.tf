edac3504006428c68b0085b94b07dfbfb0fcdbc3
shrookmuhamed <152062028+shrookmuhamed@users.noreply.github.com>
2024-05-06 17:00:52 +0300

    Update variables.tf

--- network/variables.tf
+++ network/variables.tf
@@ -1,16 +1,12 @@
 variable vpc_cidr {
   type = string
-  description = "description"
 }
 variable pub_subnet_cidr {
   type = string
-  description = "description"
 }
 variable priv_subnet_cidr {
   type = string
-  description = "description"
 }
 variable priv_subnet2_cidr {
   type = string
-  description = "description"
 }
