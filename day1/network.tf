8ad743f672bd03c356650704d8d6397323360736
shrookmuhamed <152062028+shrookmuhamed@users.noreply.github.com>
2024-05-07 15:53:44 +0300

    Update network.tf

--- day1/network.tf
+++ day1/network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "/home/shrouk/Documents/ITI/terraform/network"
+  source = "github.com/shrookmuhamed/Terraform_project//day1/network?ref=main"
   vpc_cidr=var.vpc_cidr
   pub_subnet_cidr=var.pub_subnet_cidr
   priv_subnet_cidr=var.priv_subnet_cidr
