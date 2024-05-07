0c070b3bbf0edbbf34da0abdf617283c42591663
shrookmuhamed <152062028+shrookmuhamed@users.noreply.github.com>
2024-05-07 15:35:56 +0300

    Update output.tf

--- day1/output.tf
+++ day1/output.tf
@@ -6,3 +6,7 @@ output "db_password" {
   value     = random_password.password.result
   sensitive = true
 }
+output "private_key" {
+  value     = tls_private_key.key1.private_key_pem
+  sensitive = true
+}
