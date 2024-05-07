329e71be80e9b7058e5a3de5eebf231ffebde8f1
shrookmuhamed <152062028+shrookmuhamed@users.noreply.github.com>
2024-05-07 15:47:12 +0300

    Add files via upload

---
+++ day1/network/variables.tf
@@ -0,0 +1,16 @@
+variable vpc_cidr {
+  type = string
+  
+}
+variable pub_subnet_cidr {
+  type = string
+  
+}
+variable priv_subnet_cidr {
+  type = string
+ 
+}
+variable priv_subnet2_cidr {
+  type = string
+ 
+}
