78af2f0c9fc435e009640167424293d8644319db
shrookmuhamed <152062028+shrookmuhamed@users.noreply.github.com>
2024-05-07 14:01:30 +0300

    Update securitygroups.tf

--- day1/securitygroups.tf
+++ day1/securitygroups.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "ssh_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
   tags = {
-    Name = "sg1"
+    Name = "sg1_ssh"
   }
 
 }
@@ -49,7 +49,28 @@ resource "aws_security_group" "ssh-3000" {
     ipv6_cidr_blocks = ["::/0"]
   }
   tags = {
-    Name = "sg2"
+    Name = "sg2_3000"
+  }
+}
+resource "aws_security_group" "rds_sec_group" {
+  name        = "rds_sec_group"
+  description = "Allow port 3306"
+  vpc_id      = module.network.myvpc_id
+
+  ingress {
+    description = "3306 ingress"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 resource "aws_security_group" "redis_sec_group" {
@@ -65,11 +86,14 @@ resource "aws_security_group" "redis_sec_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    description = "6379 ingress"
-    from_port   = 6379
-    to_port     = 6379
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "sg_redis"
   }
 }
