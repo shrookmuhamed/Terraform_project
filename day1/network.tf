c643df4bf17cde7bf70729d32ed03cc0818e0042
shrookmuhamed <152062028+shrookmuhamed@users.noreply.github.com>
2024-05-07 19:42:26 +0300

    Update network.tf

--- day1/network.tf
+++ day1/network.tf
@@ -1,7 +1,7 @@
 module "network" {
-  source = "${path.module}/network"
-  vpc_cidr=var.vpc_cidr
-  pub_subnet_cidr=var.pub_subnet_cidr
-  priv_subnet_cidr=var.priv_subnet_cidr
-  priv_subnet2_cidr=var.priv_subnet2_cidr
+  source = path.module + "/network"
+  vpc_cidr = var.vpc_cidr
+  pub_subnet_cidr = var.pub_subnet_cidr
+  priv_subnet_cidr = var.priv_subnet_cidr
+  priv_subnet2_cidr = var.priv_subnet2_cidr
 }
