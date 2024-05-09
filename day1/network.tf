fb352a2cf4e4552d30405317255e952768007d95
shrookmuhamed <152062028+shrookmuhamed@users.noreply.github.com>
2024-05-09 12:56:15 +0300

    Delete day1 directory

--- day1/network.tf
+++
@@ -1,7 +0,0 @@
-module "network" {
-  source = "git::https://github.com/shrookmuhamed/Terraform_project.git//day1/network?ref=main"
-  vpc_cidr = var.vpc_cidr
-  pub_subnet_cidr = var.pub_subnet_cidr
-  priv_subnet_cidr = var.priv_subnet_cidr
-  priv_subnet2_cidr = var.priv_subnet2_cidr
-}
